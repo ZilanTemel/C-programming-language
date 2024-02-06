767267b632e464bcc29c0471081fac06a26cb47c
ZilanTemel <152874666+ZilanTemel@users.noreply.github.com>
2024-02-06 23:19:24 +0300

    Sum function

---
+++ Functions/sum function/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int sum(int s1,int s2)
+{
+	int s3;
+	s3=s1+s2;
+	return s3;
+}
+
+
+	
+	int main (){
+	int t ;
+	t=sum(4,5);
+	printf("%d",t);
+	
+	
+	
+	
+	
+	return 0;
+}
