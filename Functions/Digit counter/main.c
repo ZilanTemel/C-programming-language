7f07eb9d75d16892bb26b2935c21568f5af61fc2
ZilanTemel <152874666+ZilanTemel@users.noreply.github.com>
2024-03-01 10:47:57 +0300

    An recursive function was programmed that counts the digits

---
+++ Functions/Digit counter/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void digitCounter(int x);
+
+int main() {
+    int n;
+    printf("Please input the number: ");
+    scanf("%d", &n);
+    digitCounter(n);
+    return 0;
+}
+
+int counter = 0;
+void digitCounter(int x) {
+    if (x <= 0) {
+        
+        printf("There are %d digit(s).\n", counter);
+        return;
+    } else {
+        counter++;
+        digitCounter(x / 10);
+    }
+}
+
