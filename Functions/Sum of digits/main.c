679400bd1bbbc683c465704ca8f6bfe134d76c91
ZilanTemel <152874666+ZilanTemel@users.noreply.github.com>
2024-03-01 11:27:48 +0300

    A recursive program was programmed that sums the digits in the input

---
+++ Functions/Sum of digits/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumOfDigits(int x);
+int main() 
+{
+	int n,result;
+	printf("please enter the number ");
+	scanf("%d",&n);
+	result =sumOfDigits(n);
+	printf("sum of entered digits are = %d ",result);
+	return 0;
+}
+int sumOfDigits(int x){
+	if(x<=0)
+	return 0;
+	else {
+		return x%10 +sumOfDigits(x/10);
+	}
+} 
