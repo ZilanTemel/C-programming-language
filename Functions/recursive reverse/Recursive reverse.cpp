f53a51257d21fdbac789458ffc23e0fa563b8719
ZilanTemel <152874666+ZilanTemel@users.noreply.github.com>
2024-03-09 16:49:19 +0300

    recursive program that reverses string

---
+++ Functions/recursive reverse/Recursive reverse.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+#define max 100
+
+void reverseString(char str[], int start, int end) {
+    if (start >= end) {
+        return;
+    }
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    reverseString(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[max];
+    printf("Please enter the string: ");
+    scanf("%s", str);
+
+    int length = strlen(str);
+    reverseString(str, 0, length - 1);
+
+    printf("The reversed string: %s\n", str);
+    return 0;
+}
+
