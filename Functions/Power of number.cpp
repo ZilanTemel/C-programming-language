68b6c0c503cf38eca47a064dba4259dc53f17022
ZilanTemel <152874666+ZilanTemel@users.noreply.github.com>
2024-04-01 21:21:51 +0300

    add power of number

---
+++ Functions/Power of number.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ 
+ int CalcuOfPower(int x,int y)
+{
+    int result=1;
+    if(y == 0) return result;
+    result=x*(CalcuOfPower(x,y-1));  
+}
+int main()
+{
+    int n,pwr;
+    int result;
+     
+    printf(" Please enter the base  value : ");
+    scanf("%d",&n);
+     
+    printf(" Please enter the value of power : ");
+    scanf("%d",&pwr);
+     
+    result=CalcuOfPower(n,pwr);
+     
+    printf(" The value of %d to the power of %d is : %ld\n\n",n,pwr,result);
+     
+    return 0;
+}
+
